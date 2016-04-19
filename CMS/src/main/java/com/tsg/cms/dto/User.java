efd291ee6ef8ceec143dc7d61fdd3d1ec6c9c236
Levi Schneider <levi.schneider2010@gmail.com>
2016-04-19 07:28:40 -0400

    AKPALOALTO-18 first user dao iteration

---
+++ CMS/src/main/java/com/tsg/cms/dto/User.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsg.cms.dto;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author apprentice
+ */
+public class User {
+    private String displayName;
+    private String passwordSalt;
+    private String passwordHash;
+    private String siteRole;
+    private String userName;
+    private int userId;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.displayName);
+        hash = 59 * hash + Objects.hashCode(this.passwordSalt);
+        hash = 59 * hash + Objects.hashCode(this.passwordHash);
+        hash = 59 * hash + Objects.hashCode(this.siteRole);
+        hash = 59 * hash + Objects.hashCode(this.userName);
+        hash = 59 * hash + this.userId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (this.userId != other.userId) {
+            return false;
+        }
+        if (!Objects.equals(this.displayName, other.displayName)) {
+            return false;
+        }
+        if (!Objects.equals(this.passwordSalt, other.passwordSalt)) {
+            return false;
+        }
+        if (!Objects.equals(this.passwordHash, other.passwordHash)) {
+            return false;
+        }
+        if (!Objects.equals(this.siteRole, other.siteRole)) {
+            return false;
+        }
+        if (!Objects.equals(this.userName, other.userName)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getSiteRole() {
+        return siteRole;
+    }
+
+    public void setSiteRole(String siteRole) {
+        this.siteRole = siteRole;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
