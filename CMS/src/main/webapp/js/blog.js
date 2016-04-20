6810cb1588732ddb0f5a0579a4cc343ca7b2ad2b
Hank Stocker <hstocker@gmail.com>
2016-04-20 13:20:14 -0400

    AKPALOALTO-4 Iterate through and display a list of blog posts with jquery; bare minimum sketch with sample data

---
+++ CMS/src/main/webapp/js/blog.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var samplePosts = [
+    post1 = {
+        title: "stuff",
+        body: "Here's some custom content!",
+        author: "ted bob"
+    },
+    post2 = {
+        title: "more stuff",
+        body: "Here's some extra special custom content!",
+        author: "billy bob"
+    },
+    post3 = {
+        title: "this stuff",
+        body: "<h1>Here's some super special custom content!</h1>",
+        author: "bob bob"
+    },
+    post4 = {
+        title: "super amazing title",
+        body: "super amazing content here dave",
+        author: "dunno lol"
+    }
+    
+    
+]
+
+$(document).ready(function () {
+    populateBlogPosts(samplePosts);
+
+})
+
+        function populateBlogPosts(data, status) {
+            var blogPanel = $('#blog-post-display');
+
+            $.each(data, function (index, post) {
+                blogPanel
+                        .append($('<div>')
+                                .addClass("panel panel-default")
+                                .append('<div>')
+                                .addClass("panel-body")
+                                .append(post.body));
+            });
+        }
+
--- CMS/src/main/webapp/jsp/sidebar.jsp
+++ CMS/src/main/webapp/jsp/sidebar.jsp
@@ -1,12 +1,12 @@
 <!--<div style="background-color: royalblue">Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/>Sidebar<br/></div>-->
 <li>
-    <a href="#">Home</a>
+    <a href="${pageContext.request.contextPath}/home">Home</a>
 </li>
 <li>
-    <a href="#">Blog</a>
+    <a href="${pageContext.request.contextPath}/blog">Blog</a>
 </li>
 <li>
-    <a href="#">Articles</a>
+    <a href="${pageContext.request.contextPath}/articles">Articles</a>
 </li>
 <li>
     <a href="#">Content-1</a>
@@ -19,4 +19,10 @@
 </li>
 <li>
     <a href="#">Contact Us</a>
+</li>
+<li>
+    <a href="${pageContext.request.contextPath}/admin">Admin</a>
+</li>
+<li>
+    
 </li>
