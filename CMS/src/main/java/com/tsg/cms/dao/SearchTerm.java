638caac94da9fab80162deeea713fc4196a9e4ca
Levi Schneider <levi.schneider2010@gmail.com>
2016-04-20 00:06:06 -0400

    AKPALOALTO-54 adding SearchTerm Enum

---
+++ CMS/src/main/java/com/tsg/cms/dao/SearchTerm.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsg.cms.dao;
+
+/**
+ *
+ * @author apprentice
+ */
+public enum SearchTerm {
+    TITLE, END_DATE, START_DATE, DATE_SUBMITTED, POST_TYPE, STATUS
+}
