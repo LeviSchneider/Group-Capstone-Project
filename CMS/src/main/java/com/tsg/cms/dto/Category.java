c28b9bb36ce5b6207c6d1d0982c29ca7cfdb8240
apprentice <mugoclan87@gmail.com>
2016-04-21 15:35:50 -0400

    AKPALOALTO-59 implemented js, jsp, controller and Dao for Categories options

---
+++ CMS/src/main/java/com/tsg/cms/dto/Category.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsg.cms.dto;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author apprentice
+ */
+public class Category {
+    private int categoryId;
+    private String categoryName;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.categoryId;
+        hash = 47 * hash + Objects.hashCode(this.categoryName);
+        hash = 47 * hash + Objects.hashCode(this.categoryPosts);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (this.categoryId != other.categoryId) {
+            return false;
+        }
+        if (!Objects.equals(this.categoryName, other.categoryName)) {
+            return false;
+        }
+        if (!Objects.equals(this.categoryPosts, other.categoryPosts)) {
+            return false;
+        }
+        return true;
+    }
+    private List<Content> categoryPosts;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public List<Content> getCategoryPosts() {
+        return categoryPosts;
+    }
+
+    public void setCategoryPosts(List<Content> categoryPosts) {
+        this.categoryPosts = categoryPosts;
+    }
+    
+    
+}
--- CMS/src/test/java/com/tsg/cms/dao/ContentDbDaoImplTest.java
+++ CMS/src/test/java/com/tsg/cms/dao/ContentDbDaoImplTest.java
@@ -5,9 +5,11 @@
  */
 package com.tsg.cms.dao;
 
+import com.tsg.cms.dto.Category;
 import com.tsg.cms.dto.Content;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +20,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -27,27 +30,58 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class ContentDbDaoImplTest {
 
     private ContentDbDao Dao;
-    
+    private CategoryDAO categoryDAO;
     private Content C1;
     private Content C2;
     private Content C3;
-    
+
+    private List<Category> cList1;
+    private List<Category> cList2;
+    private List<Category> cList3;
+
+    private Category cat1;
+    private Category cat2;
+    private Category cat3;
+
     public ContentDbDaoImplTest() {
     }
-    
+
     @Before
     public void setUp() throws ParseException {
-        
+
         ApplicationContext ctx = new ClassPathXmlApplicationContext("test-applicationContext.xml");
         Dao = ctx.getBean("ContentDbDao", ContentDbDao.class);
-        
+
         JdbcTemplate cleaner = (JdbcTemplate) ctx.getBean("jdbcTemplate");
         cleaner.execute("delete from blogPosts");
-       
+
+        ApplicationContext ctx2 = new ClassPathXmlApplicationContext("test-applicationContext.xml");
+        categoryDAO = ctx2.getBean("CategoryDAO", CategoryDAO.class);
+
+        JdbcTemplate cleaner2 = (JdbcTemplate) ctx2.getBean("jdbcTemplate");
+        cleaner.execute("delete from categories");
+
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date = format.parse("12-25-2016 00:00:00");
+
+        cList1 = new ArrayList<>();
+        cat1 = new Category();
+        cat1.setCategoryName("News");
+        cat1 = categoryDAO.addCategory(cat1);
+
+        cat2 = new Category();
+        cat2.setCategoryName("Sales");
+        cat2 = categoryDAO.addCategory(cat2);
+
+        cat3 = new Category();
+        cat3.setCategoryName("Cheese");
+        cat3 = categoryDAO.addCategory(cat3);
+
+        cList1.add(cat1);
+        cList1.add(cat2);
+
         C1 = new Content();
-        
+
         C1.setDateSubmitted(date);
         C1.setStartDate(date);
         C1.setEndDate(date);
@@ -56,9 +90,9 @@ public class ContentDbDaoImplTest {
         C1.setUserIdFK(11);
         C1.setStatus("Published");
         C1.setPostType("International");
-        
+
         C2 = new Content();
-        
+
         C2.setDateSubmitted(date);
         C2.setStartDate(date);
         C2.setEndDate(date);
@@ -67,10 +101,9 @@ public class ContentDbDaoImplTest {
         C2.setUserIdFK(22);
         C2.setStatus("PendingForApproval");
         C2.setPostType("Local");
-        
-        
+
         C3 = new Content();
-       
+
         C3.setDateSubmitted(date);
         C3.setStartDate(date);
         C3.setEndDate(date);
@@ -79,23 +112,46 @@ public class ContentDbDaoImplTest {
         C3.setUserIdFK(33);
         C3.setStatus("Draft");
         C3.setPostType("Local");
-        
+
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     /**
      * Test of addContent method, of class ContentDbDaoImpl.
      */
     @Test
-    public void testAddContent() {
-       Dao.addContent(C1);
-       Content fromDb = Dao.getContentById(C1.getPostId());
-       C1.setPostId(fromDb.getPostId());
-        assertEquals(C1, fromDb);
-        
+    public void testAddContentDuplicateCategory() {
+        try {
+            C1 = Dao.addContent(C1);
+            categoryDAO.addCategory(cat1, C1.getPostId());
+            Content fromDb = Dao.getContentById(C1.getPostId());
+            assertEquals(C1, fromDb);
+        } catch (DuplicateKeyException e) {
+            System.out.println("Duplicate Key");
+            fail();
+        }
+
+    }
+
+    @Test
+    public void testAddContentUniqueCategory() {
+        try {
+            Category Cat4 = new Category();
+            Cat4.setCategoryName("Unique");
+            cList1.add(Cat4);
+
+            C1 = Dao.addContent(C1);
+            Cat4 = categoryDAO.addCategory(Cat4, C1.getPostId());
+            Content fromDb = Dao.getContentById(C1.getPostId());
+            assertEquals(C1, fromDb);
+        } catch (DuplicateKeyException e) {
+            System.out.println("Duplicate Key");
+            fail();
+        }
+
     }
 
     /**
@@ -117,14 +173,14 @@ public class ContentDbDaoImplTest {
     @Test
     public void testUpdateContent() {
         C3 = Dao.addContent(C3);
-        
+
         C3.setTitle("M");
         Dao.updateContent(C3);
-        
+
 //        C3.setPostId(0);
         Content fromDb = Dao.getContentById(C3.getPostId());
         C3.setPostId(fromDb.getPostId());
-        
+
         assertEquals(C3, fromDb);
     }
 
@@ -136,10 +192,10 @@ public class ContentDbDaoImplTest {
         Dao.addContent(C1);
         Dao.addContent(C2);
         Dao.addContent(C3);
-        
+
         List<Content> cList = Dao.getAllContent();
         assertEquals(cList.size(), 3);
-        
+
     }
 
     /**
@@ -150,31 +206,29 @@ public class ContentDbDaoImplTest {
         Dao.addContent(C1);
         Dao.addContent(C2);
         Dao.addContent(C3);
-        
+
         Map<SearchTerm, String> criteria = new HashMap<>();
         criteria.put(SearchTerm.TITLE, "News");
         List<Content> cList = Dao.searchContent(criteria);
         assertEquals(2, cList.size());
-        
+
         criteria.put(SearchTerm.POSTTYPE, "International");
         cList = Dao.searchContent(criteria);
         Content fromDb = cList.get(0);
         C1.setPostId(fromDb.getPostId());
         assertEquals(C1, fromDb);
         assertEquals(1, cList.size());
-        
+
         criteria = new HashMap<>();
         criteria.put(SearchTerm.STATUS, "Draft");
         cList = Dao.searchContent(criteria);
         assertEquals(1, cList.size());
-        
+
         criteria = new HashMap<>();
         criteria.put(SearchTerm.STATUS, "Published");
         cList = Dao.searchContent(criteria);
         assertEquals(C1, cList.get(0));
-        
-        
-        
+
     }
-    
+
 }
