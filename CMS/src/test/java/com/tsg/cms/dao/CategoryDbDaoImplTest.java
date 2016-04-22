bc2a7b7e1321af4cda3c9eb3c3d57655e2f33ff1
Hank Stocker <hstocker@gmail.com>
2016-04-22 10:08:26 -0400

    Fix foreign key conflict in categorydao tests

--- CMS/src/test/java/com/tsg/cms/dao/CategoryDbDaoImplTest.java
+++ CMS/src/test/java/com/tsg/cms/dao/CategoryDbDaoImplTest.java
@@ -5,7 +5,11 @@
  */
 package com.tsg.cms.dao;
 
+import com.tsg.cms.dto.BlogPost;
 import com.tsg.cms.dto.Category;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
@@ -23,31 +27,74 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class CategoryDbDaoImplTest {
     
     private CategoryDbDao Dao;
+    private BlogPostDbDao blogDao;
+    
     private Category C1;
     private Category C2;
     private Category C3;
-    int P1, P2, P3;
+    private BlogPost blogPost1, blogPost2, blogPost3;
     
     public CategoryDbDaoImplTest() {
     }
     
     @Before
-    public void setUp() {
+    public void setUp() throws ParseException {
         
         ApplicationContext ctx = new ClassPathXmlApplicationContext("test-applicationContext.xml");
         Dao = ctx.getBean("CategoryDbDao", CategoryDbDao.class);
+        blogDao = ctx.getBean("BlogPostDbDao", BlogPostDbDao.class);
         
         JdbcTemplate cleaner = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+                cleaner.execute("delete from categoriesPostsBridge");
         cleaner.execute("delete from categories");
-        P1 = 1111;
+        
+                cleaner.execute("delete from blogPosts");
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = format.parse("12-25-2016 00:00:00");
+        blogPost1 = new BlogPost();
+
+        blogPost1.setDateSubmitted(date);
+        blogPost1.setStartDate(date);
+        blogPost1.setEndDate(date);
+        blogPost1.setTitle("News");
+        blogPost1.setPostBody("Q");
+        blogPost1.setUserIdFK(11);
+        blogPost1.setStatus("Published");
+        blogPost1.setPostType("International");
+
+        blogPost2 = new BlogPost();
+
+        blogPost2.setDateSubmitted(date);
+        blogPost2.setStartDate(date);
+        blogPost2.setEndDate(date);
+        blogPost2.setTitle("Ads");
+        blogPost2.setPostBody("B");
+        blogPost2.setUserIdFK(22);
+        blogPost2.setStatus("PendingForApproval");
+        blogPost2.setPostType("Local");
+
+        blogPost3 = new BlogPost();
+
+        blogPost3.setDateSubmitted(date);
+        blogPost3.setStartDate(date);
+        blogPost3.setEndDate(date);
+        blogPost3.setTitle("News");
+        blogPost3.setPostBody("F");
+        blogPost3.setUserIdFK(33);
+        blogPost3.setStatus("Draft");
+        blogPost3.setPostType("Local");
+
+        blogDao.addBlogPost(blogPost1);
+        blogDao.addBlogPost(blogPost2);
+        blogDao.addBlogPost(blogPost3);
+        
         C1 = new Category();
         C1.setCategoryName("category1");
         
-        P2 = 2222;
         C2 = new Category();
         C2.setCategoryName("category2");
         
-        P3 = 3333;
         C3 = new Category();
         C3.setCategoryName("category3");
     }
@@ -64,7 +111,7 @@ public class CategoryDbDaoImplTest {
     public void testAddCategory() {
         
         C1 = Dao.addCategory(C1);
-        Dao.addCategoryAndPostToBridge(C1, P1);
+        Dao.addCategoryAndPostToBridge(C1, blogPost1.getPostId());
         Category fromDb = Dao.getCategoryById(C1.getCategoryId());
         C1.setCategoryId(fromDb.getCategoryId());
         assertEquals(C1, fromDb);
@@ -79,7 +126,7 @@ public class CategoryDbDaoImplTest {
     public void testRemoveCategory() {
         
         C2 = Dao.addCategory(C2);
-        Dao.addCategoryAndPostToBridge(C2, P2);
+        Dao.addCategoryAndPostToBridge(C2, blogPost2.getPostId());
         Category fromDb = Dao.getCategoryById(C2.getCategoryId());
         C2.setCategoryId(fromDb.getCategoryId());
         
@@ -96,7 +143,7 @@ public class CategoryDbDaoImplTest {
     public void testUpdateCategory() {
         
         C3 = Dao.addCategory(C3);
-        Dao.addCategoryAndPostToBridge(C3, P3);
+        Dao.addCategoryAndPostToBridge(C3, blogPost3.getPostId());
         
         C3.setCategoryName("CategoryTest");
         Dao.updateCategory(C3);
@@ -114,11 +161,11 @@ public class CategoryDbDaoImplTest {
     public void testGetAllCategories() {
         
         C1 = Dao.addCategory(C1);
-        Dao.addCategoryAndPostToBridge(C1, P1);
+        Dao.addCategoryAndPostToBridge(C1, blogPost1.getPostId());
         C2 = Dao.addCategory(C2);
-        Dao.addCategoryAndPostToBridge(C2, P2);
+        Dao.addCategoryAndPostToBridge(C2, blogPost2.getPostId());
         C3 = Dao.addCategory(C3);
-        Dao.addCategoryAndPostToBridge(C3, P3);
+        Dao.addCategoryAndPostToBridge(C3, blogPost3.getPostId());
         
         List<Category> cList = Dao.getAllCategories();
         assertEquals(3, cList.size());
@@ -132,10 +179,10 @@ public class CategoryDbDaoImplTest {
     @Test
     public void testSearchCategory() {
         
-        Dao.addCategoryAndPostToBridge(C1, P1);
-        Dao.addCategoryAndPostToBridge(C2, P2);
-        Dao.addCategoryAndPostToBridge(C3, P3);
-        
+//        Dao.addCategoryAndPostToBridge(C1, blogPost1.getPostId());
+//        Dao.addCategoryAndPostToBridge(C2, blogPost2.getPostId());
+//        Dao.addCategoryAndPostToBridge(C3, blogPost3.getPostId());
+//        
         
         
     }
