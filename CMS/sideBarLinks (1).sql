07c5b602b7d0ec20a7ffd3fe7b5dfeb2c6cd5910
David Johnson <johnson.1832@gmail.com>
2016-04-28 17:27:19 -0400

    AKPALOALTO-42 Loading all sidebar links from DB

--- CMS/sideBarLinks (1).sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `sideBarLinks` (`sideBarLinksId`, `staticPageName`, `url`, `position`, `isDefaultLink`) VALUES
-(1, 'About Us', 'a_big_old_test', 5, 0),
-(4, 'Home', 'home', 0, 1),
-(5, 'Blog', 'blog', 1, 1),
-(6, 'Admin', 'admin', 2, 1),
-(7, 'Create Blog Entry', 'tinymce', 3, 1),
-(8, 'Create Static Page', 'pageTinyMCE', 4, 1);
